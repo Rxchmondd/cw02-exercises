388d392a9edfae636886bac6f3a2200fdb374116
Richmond <yungthrowawaynocap@gmail.com>
2020-03-07 13:15:51 -0500

    richandlukefinished

---
+++ exercise_2.py
@@ -0,0 +1,14 @@
+import random
+
+time = 8
+
+if time < 9:
+    print("Morning is wonderful. Its only drawback is that it comes at such an inconvenient time of day.")
+elif time <= 4:
+    print("Working hard or hardly working?")
+elif time < 8:
+    print("How did it get so late so soon?")
+elif time < 10:
+    print("A day without sunshine is like, you know, night.")
+else:
+    print("Burning the midnight oil!")
